0e48cb4e00fea1674e2951af4368c2c77b1b1eaf
annapurnachinta <annapurna@itversity.in>
2020-12-22 12:33:10 +0530

    todo list in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoListAngular';
+  public newTask: string; 
+  errorMsg: string;
+  STORAGE_KEY = 'local_todolist';
+
+  public items = JSON.parse(localStorage.getItem(this.STORAGE_KEY)) || [];
+
+  public addTask(){
+    if(this.newTask == ''){
+
+    }
+    else if(this.items.indexOf(this.newTask) == -1){
+      this.items.push(this.newTask)
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.items))
+      this.newTask = '';
+      this.errorMsg = '';
+    }
+    else{
+      this.errorMsg = 'This item is already in your Todo List.';
+    }
+}
+
+  public deleteTask(index: number){
+      // this.items = this.items.filter(i => i !== index)
+      this.items.splice(index, 1)
+      localStorage.removeItem(this.items)
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.items))
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StorageServiceModule } from 'ngx-webstorage-service'
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    StorageServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
